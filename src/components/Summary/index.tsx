a0cb76ac5edb868c9ce3c6b7820608e05433388c
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-02-08 19:15:43 -0300

    feat: add sumary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./style";
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg  from '../../assets/total.svg';
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="income" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="income" />
+        </header>
+        <strong>- R$1000,00</strong>
+      </div>
+      <div className="hight-light-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="income" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+    </Container>
+  );
+}
