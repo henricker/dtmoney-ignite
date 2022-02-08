b149baf0c1a3b3ab9305fee65052f44ecffc2652
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-02-08 19:16:14 -0300

    feat: add transactions table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-function App() {
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+
+export function App() {
   return (
-    <div className="App">
-      Hello world!
-    </div>
+    <>
+      <Header/>
+      <Dashboard/>
+      <GlobalStyle />
+    </>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createServer } from 'miragejs'
+import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => [
+      {
+        id: 1,
+        title: 'Desenvolvimento de website',
+        amount: 12000,
+        type: 'Desenvolvimento',
+        createdAt: new Date()
+      },
+      {
+        id: 2,
+        title: 'Aluguel',
+        amount: 1100,
+        type: 'Casa',
+        createdAt: new Date()
+      }
+    ])
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
